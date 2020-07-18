7a9815da84ee63aa44be00a990c3d68ec1f659fe
meisiman <meisiman@qq.com>
2020-07-18 15:15:42 +0800

    finish

---
+++ 剑指offer/Offer68_2.java
@@ -0,0 +1,20 @@
+package offer;
+
+/**
+ * @author Mei Siman
+ * @date 2020/7/18 15:05
+ *
+ * 面试题68 - II. 二叉树的最近公共祖先
+ *
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ */
+public class Offer68_2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||root==p||root==q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left==null) return right;
+        if(right==null) return left;
+        return root;
+    }
+}
