be039d7a757f8b834f17b7063295f5afb7de7c39
meisiman <meisiman@qq.com>
2020-08-06 17:10:30 +0800

    continue

---
+++ leetcode/Test0075.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+/**
+ * @author Mei Siman
+ * @date 2020/8/6 12:38
+ *
+ * 75. 颜色分类
+ *
+ * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ *
+ * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ *
+ */
+public class Test0075 {
+    public void sortColors(int[] nums) {
+        int len = nums.length;
+        if (len < 2) return;
+        int zero = 0;
+        int two = len;
+        int i = 0;
+        while (i < two) {
+            if (nums[i] == 0) {
+                swap(nums, i, zero);
+                zero++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                two--;
+                swap(nums, i, two);
+            }
+        }
+    }
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
