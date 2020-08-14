6c897f3839f45772d14fa5fc67a53c312b2f1a0f
meisiman <meisiman@qq.com>
2020-08-14 18:48:53 +0800

    continue

---
+++ leetcode/Test0105.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author Mei Siman
+ * @date 2020/8/14 12:42
+ *
+ * 105. 从前序与中序遍历序列构造二叉树
+ */
+public class Test0105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+}
