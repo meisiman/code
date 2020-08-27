a8a6ef5d8afa0988a584456ee7f77d2075b6304a
meisiman <meisiman@qq.com>
2020-08-27 16:42:36 +0800

    continue

---
+++ leetcode/Test0207.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Mei Siman
+ * @date 2020/8/27 13:14
+ *
+ * 207. 课程表
+ *
+ * 你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+ *
+ * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+ *
+ * 给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+ */
+public class Test0207 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegrees = new int[numCourses];
+        List<List<Integer>> adjacency = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++)
+            adjacency.add(new ArrayList<>());
+        // Get the indegree and adjacency of every course.
+        for(int[] cp : prerequisites) {
+            indegrees[cp[0]]++;
+            adjacency.get(cp[1]).add(cp[0]);
+        }
+        // Get all the courses with the indegree of 0.
+        for(int i = 0; i < numCourses; i++)
+            if(indegrees[i] == 0) queue.add(i);
+        // BFS TopSort.
+        while(!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            for(int cur : adjacency.get(pre))
+                if(--indegrees[cur] == 0) queue.add(cur);
+        }
+        return numCourses == 0;
+    }
+}
