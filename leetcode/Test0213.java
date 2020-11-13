1abe9e6181490c0049efcf4418f1344ca04d3d2d
meisiman <meisiman@qq.com>
2020-11-13 10:51:16 +0800

    2020.11.12

---
+++ leetcode/Test0213.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author Mei Siman
+ * @date 2020/11/12 11:41
+ *
+ * 213. 打家劫舍 II
+ *
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。
+ * 这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。
+ * 同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+ *
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，能够偷窃到的最高金额。
+ *
+ */
+public class Test0213 {
+    public int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        if(nums.length==2) return Math.max(nums[0],nums[1]);
+        return Math.max(myRob(Arrays.copyOfRange(nums,0,nums.length-1)),
+                myRob(Arrays.copyOfRange(nums,1,nums.length)));
+    }
+
+    public int myRob(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+}
